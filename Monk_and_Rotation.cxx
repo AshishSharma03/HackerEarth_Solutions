883515debeee0e25c304cc5b811d088e01608116
Ashish Sharma <30octaksharma@gmail.com>
2021-07-27 13:39:53 +0530

    Monk and rotation

---
+++ Monk_and_Rotation.cxx
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int a[100001];
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t,n,k;
+    cin>>t;
+    while(t--){
+
+        cin>>n
+           >>k;
+
+        while(k >=n)k -= n;
+
+        for (int i {0}; i < n; i++) {
+			cin >> a[i];
+		}
+
+        for(int i{n-k}; i<n; i++ ){
+            cout<<a[i]<<" ";
+        }
+        for(int i {0}; i<n-k; i++ ){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }      
+    return 0;
+}
